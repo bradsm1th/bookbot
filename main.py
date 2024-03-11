50174ea33f6d315f99e47466ea5757c071b96ed1
Brad Smith <bradsmith@duck.com>
2024-03-11 16:46:28 -0400

    Add 'count chars' dict, too

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     # count words
     total_words = count_words(text)
     print(f"There are roughly {total_words} words")
+    # count letters
+    chars = tot_up_letters(text) # a { } of lowercase letters
+    for ch in chars:
+        print(f"{ch}: {chars[ch]}")
 
 def get_text_of_book(path):
     with open(path) as file:
@@ -17,7 +21,18 @@ def count_words(string):
     words = string.split()
     return len(words)
 
-def tot_up_letters(book_text_string -> str):
-    pass
+
+def tot_up_letters(book_text_string: str):
+    counted_chars = {}
+    # make all chars lowercase
+    lowercase_text = book_text_string.lower()
+    for ch in lowercase_text:
+        if ch in counted_chars:
+            counted_chars[ch] += 1
+        else:
+            counted_chars[ch] = 1
+
+
+    return counted_chars
 
 main()
