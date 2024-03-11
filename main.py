49bca1fa74608042023332310063c31d9c5d1643
Brad Smith <bradsmith@duck.com>
2024-03-11 19:48:02 -0400

    Add sorted report function to finish

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ def main():
     path_of_book = 'books/frankenstein.txt'
     # get full text
     text = get_text_of_book(path_of_book)
-    print(text)
     # count words
     total_words = count_words(text)
-    print(f"There are roughly {total_words} words")
     # count letters
     chars = tot_up_letters(text) # a { } of lowercase letters
-    for ch in chars:
-        print(f"{ch}: {chars[ch]}")
+    # test "one dict to list of dicts"
+    list_of_letters = to_list_of_dicts(chars)
+    # print the report
+    print_report(total_words, list_of_letters, path_of_book)
 
-def get_text_of_book(path):
+
+# ============================
+# all the helper functions 
+# ============================
+def get_text_of_book(path: str):
     with open(path) as file:
         file_contents = file.read()
     return file_contents
@@ -27,12 +31,43 @@ def tot_up_letters(book_text_string: str):
     # make all chars lowercase
     lowercase_text = book_text_string.lower()
     for ch in lowercase_text:
-        if ch in counted_chars:
-            counted_chars[ch] += 1
-        else:
-            counted_chars[ch] = 1
-
-
+        if ch.isalpha():
+            if ch in counted_chars:
+                counted_chars[ch] += 1
+            else:
+                counted_chars[ch] = 1
     return counted_chars
 
+def sort_it(dict):
+    return dict['count']
+
+def to_list_of_dicts(dict):
+    letter_dicts = []
+    for entry in dict:
+        letter_dicts.append({
+            'name': entry,
+            'count': dict[entry]
+        })
+
+    # sort it
+    letter_dicts.sort(key=sort_it, reverse=True)
+    return letter_dicts
+
+
+def print_report(word_count: int, list_of_chars_dicts: list, title: str):
+    print()
+    print(f"  Start  ".center(40, "*"))
+    print()
+    print(f"File:\t{title}")
+    print(f"Words:\t{word_count}")
+    print()
+
+    for entry in list_of_chars_dicts:
+        print(f"The '{entry["name"]}' character was found {entry["count"]} times ")
+    
+    print()
+    print(f"  End  ".center(40, "*"))
+    print()
+
+    
 main()
