21b1ca76a3788cea2f961490fd52cf764773dce2
Brad Smith <bradsmith@duck.com>
2024-03-11 16:17:46 -0400

    Add 'read all text' function and main method

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+def main():
+    path_of_book = 'books/frankenstein.txt'
+    text = get_text_of_book(path_of_book)
+    print(text)
+
+
+def get_text_of_book(path):
+    with open(path) as file:
+        file_contents = file.read()
+    return file_contents
+
+
+
+
+main()
