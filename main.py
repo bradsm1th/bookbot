4a7eef52a88a95b4d1b7ba60c15757ea975960e7
brad smith <significantdustspeck@icloud.com>
2025-10-02 20:48:17 -0400

    Add 'count characters'

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from stats import count_words, count_chars
+
 def get_book_text(path):
     with open(path) as file_obj:
         text = file_obj.read()
     return text 
-
-def count_words(t_as_s):
-    return len(t_as_s.split())
-
-def main():
-    print(f"Found {count_words(get_book_text("books/frankenstein.txt"))} total words")
     
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    words = count_words(text)
+    char_count = count_chars(text)
+
+    print(f"Found {words} total words")
+    print(char_count)
+
 main()
